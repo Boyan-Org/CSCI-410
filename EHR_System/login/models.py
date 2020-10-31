f3b22d97934c0fa511142f6d5b96ae383daf8c71
Frankie <zz1763@nyu.edu>
2020-10-31 16:54:13 +0800

    first_commit

---
+++ EHR_System/login/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Login_Info(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    role = models.CharField(
+        max_length=20,
+        choices=(
+            ('admin', 'admin'),
+            ('medical_staff', 'medical staff'),
+            ('patient', 'patient'),
+        )
+    )
+
+    class Meta:
+        db_table = 'login_info'
