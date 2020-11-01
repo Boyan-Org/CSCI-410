e2929d4e609f9fbbce9e1f5e3b3de054d0504fc0
boyan <bx376@nyu.edu>
2020-11-01 13:34:46 +0800

    Revert "Co-authored-by: Hellen Wang <yw2515@nyu.edu>"
    This reverts commit 6287c716dc06e6ccb31194f4041f56fe87f8024f.

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [ 
+INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "hello",
-    "products",
 ]
 
 MIDDLEWARE = [
